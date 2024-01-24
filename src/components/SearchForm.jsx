ece1540bc31ca65a1bed026ca38cc604a8861030
dgr92 <david-gr_92@hotmail.com>
2024-01-24 20:32:56 +0100

    arreglado fallo formulario

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -7,15 +7,19 @@ export const SearchForm = () => {
 
 	const handleSearchPokemon = (e) => {
 		const searchFor = e.target.value;
-		if(pokemonDataList.length){
+		if (pokemonDataList.length) {
 			const results = pokemonDataList.filter((pokemonData) => pokemonData.pokemon.name.toLowerCase().indexOf(searchFor.toLowerCase()) > -1);
 			setSearchResults(results);
 		}
-		
-    };
+	};
+
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
 	return (
-		<form className={`search-bar`} onChange={handleSearchPokemon}>
+		<form className={`search-bar`} onChange={handleSearchPokemon} onSubmit={handleSubmit}>
 			<input type='search' name='pokemonSearch' placeholder='Buscar Pokémon...' />
 		</form>
 	);
